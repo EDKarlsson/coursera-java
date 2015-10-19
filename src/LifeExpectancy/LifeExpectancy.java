da66a293626c59e940442377867c081ebfd3562e
Dan Karlsson <dan.e.karlsson@jpl.nasa.gov>
2015-10-18 18:33:34 -0700

    Started working on the assignment, parsing out the locations from the features inside the createmarker function. Though should look into whether or not I should leave it as is and implement my own methods for this.

---
+++ src/LifeExpectancy/LifeExpectancy.java
@@ -0,0 +1,78 @@
+package LifeExpectancy;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Java app for making a map to display life expectancy around the world Created by dank on 10/17/15.
+ */
+public class LifeExpectancy extends PApplet {
+    UnfoldingMap           map;
+    HashMap<String, Float> lifeExpMap;
+    Map<String, Float>     lifeExpByCountry;
+    List<Feature>          countries;
+    List<Marker>           countryMarkers;
+
+    public void setup() {
+        // Sets the size of the application
+        size(800, 600, OPENGL);
+        // Sets the size of the map and type of map.
+        map = new UnfoldingMap(this, 50, 50, 700, 500, new Google.GoogleMapProvider());
+        MapUtils.createDefaultEventDispatcher(this, map);
+
+        // Loads a Comma Separated Values file with data about the life expectancies of countries around the world
+        lifeExpByCountry = loadLifeExpectancyFromCSV("LifeExpectancyWorldBank.csv");
+        println("Loaded " + lifeExpMap.size() + " data entries");
+
+        // Load country polygons and adds them as markers
+        countries = GeoJSONReader.loadData(this, "countries.geo.json");
+        countryMarkers = MapUtils.createSimpleMarkers(countries);
+        map.addMarkers(countryMarkers);
+
+        // Country markers are shaded according to life expectancy (only once)
+        shadeCountries();
+    }
+
+    private void shadeCountries() {
+        for (Marker marker : countryMarkers) {
+            String countryId = marker.getId();
+            if (lifeExpMap.containsKey(countryId)) {
+                float lifeExp = lifeExpMap.get(countryId);
+                int colorLevel = (int) map(lifeExp, 40, 90, 10, 255);
+                marker.setColor(color(255 - colorLevel, 100, colorLevel));
+            }
+            else {
+                marker.setColor(color(150, 150, 150));
+
+            }
+        }
+    }
+
+    private Map<String, Float> loadLifeExpectancyFromCSV(String fileName) {
+        Map<String, Float> lifeExpMap = new HashMap<String, Float>();
+        String[]           rows       = loadStrings(fileName);
+
+        for (String row : rows) {
+            String[] columns = row.split(",");
+            if (columns.length == 6 && !columns[5].equals("..")) {
+                float value = Float.parseFloat(columns[5]);
+                lifeExpMap.put(columns[4], value);
+            }
+        }
+        return lifeExpMap;
+    }
+
+    public void draw() {
+        map.draw();
+    }
+}
--- src/module3/EarthquakeCityMap.java
+++ src/module3/EarthquakeCityMap.java
@@ -1,17 +1,18 @@
 package module3;
 
 //Java utilities libraries
+
 import java.util.ArrayList;
 //import java.util.Collections;
 //import java.util.Comparator;
 import java.util.List;
 
 //Processing library
+import de.fhpotsdam.unfolding.marker.Marker;
 import processing.core.PApplet;
 
 //Unfolding libraries
 import de.fhpotsdam.unfolding.UnfoldingMap;
-import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.Google;
@@ -29,33 +30,33 @@ import parsing.ParseFeed;
  */
 public class EarthquakeCityMap extends PApplet {
 
-	// You can ignore this.  It's to keep eclipse from generating a warning.
-	private static final long serialVersionUID = 1L;
+    // You can ignore this.  It's to keep eclipse from generating a warning.
+    private static final long serialVersionUID = 1L;
 
-	// IF YOU ARE WORKING OFFLINE, change the value of this variable to true
-	private static final boolean offline = false;
+    // IF YOU ARE WORKING OFFLINE, change the value of this variable to true
+    private static final boolean offline = false;
 
-	/**
-	 * The constant THRESHOLD_MODERATE.
-	 */
-	// Less than this threshold is a light earthquake
-	public static final float THRESHOLD_MODERATE = 5;
-	/**
-	 * The constant THRESHOLD_LIGHT.
-	 */
-	// Less than this threshold is a minor earthquake
-	public static final float THRESHOLD_LIGHT = 4;
+    /**
+     * The constant THRESHOLD_MODERATE.
+     */
+    // Less than this threshold is a light earthquake
+    public static final float THRESHOLD_MODERATE = 5;
+    /**
+     * The constant THRESHOLD_LIGHT.
+     */
+    // Less than this threshold is a minor earthquake
+    public static final float THRESHOLD_LIGHT    = 4;
 
-	/**
-	 * This is where to find the local tiles, for working without an Internet connection
-	 */
-	public static String mbTilesString = "blankLight-1-3.mbtiles";
+    /**
+     * This is where to find the local tiles, for working without an Internet connection
+     */
+    public static String mbTilesString = "blankLight-1-3.mbtiles";
 
-	// The map
-	private UnfoldingMap map;
+    // The map
+    private UnfoldingMap map;
 
-	//feed with magnitude 2.5+ Earthquakes
-	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
+    //feed with magnitude 2.5+ Earthquakes
+    private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
 
     public void setup() {
         size(950, 600, OPENGL);
@@ -70,54 +71,80 @@ public class EarthquakeCityMap extends PApplet {
             //earthquakesURL = "2.5_week.atom";
         }
 
-	    map.zoomToLevel(2);
-	    MapUtils.createDefaultEventDispatcher(this, map);
+        map.zoomToLevel(2);
+        MapUtils.createDefaultEventDispatcher(this, map);
 
-	    // The List you will populate with new SimplePointMarkers
-	    List<Marker> markers = new ArrayList<Marker>();
+        // The List you will populate with new SimplePointMarkers
+        List<Marker> markers = new ArrayList<Marker>();
 
-	    //Use provided parser to collect properties for each earthquake
-	    //PointFeatures have a getLocation method
-	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+        //Use provided parser to collect properties for each earthquake
+        //PointFeatures have a getLocation method
+        List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
 
-	    // These print statements show you (1) all of the relevant properties
-	    // in the features, and (2) how to get one property and use it
-	    if (earthquakes.size() > 0) {
-	    	PointFeature f = earthquakes.get(0);
-	    	System.out.println(f.getProperties());
-	    	Object magObj = f.getProperty("magnitude");
-	    	float mag = Float.parseFloat(magObj.toString());
-	    	// PointFeatures also have a getLocation method
-	    }
+        // These print statements show you (1) all of the relevant properties
+        // in the features, and (2) how to get one property and use it
+        if (earthquakes.size() > 0) {
+            PointFeature f = earthquakes.get(0);
+            System.out.println(f.getProperties());
+            Object magObj = f.getProperty("magnitude");
+            float mag = Float.parseFloat(magObj.toString());
+            println("Magnitude : " + mag);
+            // PointFeatures also have a getLocation method
+            System.out.println(createMarker(f));
+            for (PointFeature feat : earthquakes) {
+                markers.add(createMarker(feat));
+            }
+            for (Marker mark : markers) {
+                System.out.println(mark.getLocation());
+            }
+        }
 
-	    // Here is an example of how to use Processing's color method to generate
-	    // an int that represents the color yellow.
-	    int yellow = color(255, 255, 0);
-
-	    //TODO: Add code here as appropriate
-
-
+        // Here is an example of how to use Processing's color method to generate
+        // an int that represents the color yellow.
+        int yellow = color(255, 255, 0);
+        //TODO: Add code here as appropriate
     }
 
-	// A suggested helper method that takes in an earthquake feature and
-	// returns a SimplePointMarker for that earthquake
-	// TODO: Implement this method and call it from setUp, if it helps
-	private SimplePointMarker createMarker(PointFeature feature)
-	{
-		// finish implementing and use this method, if it helps.
-		return new SimplePointMarker(feature.getLocation());
-	}
+    // A suggested helper method that takes in an earthquake feature and
+    // returns a SimplePointMarker for that earthquake
+    // TODO: Implement this method and call it from setUp, if it helps
+    private SimplePointMarker createMarker(PointFeature feature)
+    {
+        if (feature == null) {
+            return null;
+        }
 
-	public void draw() {
-	    background(10);
-	    map.draw();
-	    addKey();
-	}
+        SimplePointMarker marker     = new SimplePointMarker();
+        Object            utilObject = feature.getProperty("magnitude");
+        float             value      = Float.parseFloat(utilObject.toString());
+        println(value);
+        if (value > 4) {
+            // Moderate Earthquake
+            utilObject = feature.getProperty("location");
+            value = Float.parseFloat(utilObject.toString());
+            println(value);
+        }
+        else {
+            println("Print else");
+            // Minor
+        }
+        // finish implementing and use this method, if it helps.
+        System.out.println("Current Feature being processed: " + feature.getProperties());
 
+        // PointFeatures also have a getLocation method
+        //        return new SimplePointMarker(feature.getLocation());
+        return marker;
+    }
 
-	// helper method to draw key in GUI
-	// TODO: Implement this method to draw the key
-	private void addKey()
+    public void draw() {
+        background(10);
+        map.draw();
+        addKey();
+    }
+
+    // helper method to draw key in GUI
+    // TODO: Implement this method to draw the key
+    private void addKey()
     {
         ellipse(200, 200, 30, 30);
         // Remember you can use Processing's graphics methods here
